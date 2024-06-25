b1846665951e44a4137c7c58c2a7092c9535e154
Paul Keydel <paul.keydel@posteo.de>
2024-06-25 09:16:35 +0200

    Kommunalwahl update

--- README.md
+++ README.md
@@ -4,11 +4,16 @@ Ein kleines Python-Projekt zur geografischen Auswertung der LINKEn Wahlergebniss
 ## Wahlergebnisse auf Landkreisebene
 Zur Europawahl 2024 hat DIE LINKE im Vergleich zur letzten EU-Wahl in fast allen Landkreisen und kreisfreien Städten verloren. Einzig in Mainz ist ein minimaler Zugewinn von 0.3% der Stimmen zu beobachten. Den größten Verlust (-13%) verzeichnet Suhl. Ein Überblick über die Wahlergebnisse der Partei und über die prozentualen Verluste in den einzelnen Landkreisen:
 ![Ergebnisse 2024 und Verluste](https://github.com/PaulKeydel/EU_elect_2024_ger_map/blob/main/Linke_heatmap.svg)
+In einigen Bundesländern haben parallel Kommunalwahlen stattgefunden. Vorerst für Sachsen, Sachsen-Anhalt und Brandenburg sind die Ergebnisse der beiden Wahlen gegenübergestellt:
+![Ergebnisse 2024 plus Kommunal](https://github.com/PaulKeydel/EU_elect_2024_ger_map/blob/main/Linke_heatmap_komm.svg)
 
 ## Interaktive Karte
 Auf einer [interaktiven Karte](https://paulkeydel.github.io/EU_elect_2024_ger_map/index.html) sind weitere Unterschiede zur letzten Wahl dargestellt. Die schwarz umrandeten Landkreise markieren wo der LINKE EU-Bus den Wahlkampf unterstützt hat. Die Farbcodierung der lokalen Verluste ist in dieser Karte nicht bei null zentriert, sondern um den mittleren bundesdeutschen Verlust von -2,75% (EU-2019: 5,5%, EU-2024: 2,75%). Damit werden diejenigen Landkreise und kreisfreie Städte rötlich eingefärbt, die weniger Stimmen verloren haben als es im bundesdeutschen Mittel der Fall ist.
 
 ## Datenquellen
-* [Wahlergebnisse 2019](https://www.bundeswahlleiterin.de/dam/jcr/095b092a-780e-45e1-aca9-caafe903b126/ew19_kerg.csv)
-* [Wahlergebnisse 2024](https://www.bundeswahlleiterin.de/dam/jcr/ac86fe50-a479-4f3d-bfc1-96b007c1a66d/ew24_kerg.csv)
+* [EP Wahlergebnisse 2019](https://www.bundeswahlleiterin.de/dam/jcr/095b092a-780e-45e1-aca9-caafe903b126/ew19_kerg.csv)
+* [EP Wahlergebnisse 2024](https://www.bundeswahlleiterin.de/dam/jcr/ac86fe50-a479-4f3d-bfc1-96b007c1a66d/ew24_kerg.csv)
+* [Kommunal 24: Sachsen-Anhalt](https://wahlergebnisse.sachsen-anhalt.de/wahlen/kw24/erg/csv/kw24dat2.csv)
+* [Kommunal 24: Sachsen](https://wahlen.sachsen.de/download/Kreistag/statistik-sachsen_KW24_VE_SN_LK_KS.xlsx)
+* [Kommunal 24: Brandenburg](https://wahlergebnisse.brandenburg.de/12/200/20240609/kreistagswahl_land/DL/DL_BB_2_KW2024.xlsx)
 * [geografischer Datensatz](https://github.com/jgehrcke/covid-19-germany-gae/tree/master/geodata)
--- linke_heatmap.py
+++ linke_heatmap.py
@@ -10,6 +10,9 @@ import matplotlib.pyplot as plt
 dfgeo = gpd.read_file("DE-counties.geojson")
 df_eu19 = pd.read_csv("ew19_kerg_mod.csv", header=0, sep=';')
 df_eu24 = pd.read_csv("ew24_kerg_mod.csv", header=0, sep=';')
+df_SN24 = pd.read_csv("KW24_SN.csv", header=0, sep=';')
+df_BB24 = pd.read_csv("KW24_BB.csv", header=0, sep=';')
+df_LSA24 = pd.read_csv("KW24_LSA.csv", header=0, sep=';')
 
 #define list of relevant AGS
 relev_ags = [6635, #Waldeck-Frankenberg
@@ -32,42 +35,60 @@ relev_ags = [6635, #Waldeck-Frankenberg
              5515, #Münster, Stadt
              ]
 
-valid_votes = dict([(2019, []), (2024, [])])
-abs_votes_linke = dict([(2019, []), (2024, [])])
-electorate = dict([(2019, []), (2024, [])])
-num_voters = dict([(2019, []), (2024, [])])
+valid_votes = dict([("EU19", []), ("EU24", []), ("KOM24", [])])
+abs_votes_linke = dict([("EU19", []), ("EU24", []), ("KOM24", [])])
+electorate = dict([("EU19", []), ("EU24", []), ("KOM24", [])])
+num_voters = dict([("EU19", []), ("EU24", []), ("KOM24", [])])
 diff_votes_linke = []
-results_linke_24 = []
 lost_voters_24 = []
 for _, row in dfgeo.iterrows():
     ags = int(row["AGS"])
     assert(ags > 1000)
     if (df_eu19["Nr"] == ags).any():
-        valid_votes[2019].append(df_eu19["Gültige"].loc[df_eu19["Nr"] == ags].item())
-        abs_votes_linke[2019].append(df_eu19["DIE LINKE"].loc[df_eu19["Nr"] == ags].item())
-        electorate[2019].append(df_eu19["Wahlberechtigte"].loc[df_eu19["Nr"] == ags].item())
-        num_voters[2019].append(df_eu19["Wähler/-innen"].loc[df_eu19["Nr"] == ags].item())
+        valid_votes["EU19"].append(df_eu19["Gültige"].loc[df_eu19["Nr"] == ags].item())
+        abs_votes_linke["EU19"].append(df_eu19["DIE LINKE"].loc[df_eu19["Nr"] == ags].item())
+        electorate["EU19"].append(df_eu19["Wahlberechtigte"].loc[df_eu19["Nr"] == ags].item())
+        num_voters["EU19"].append(df_eu19["Wähler/-innen"].loc[df_eu19["Nr"] == ags].item())
     else:
         print("ERROR: AGS not found in results from 2019!")
     if (df_eu24["Nr"] == ags).any() or (ags == 16056):
         if (ags == 16056):
             #Eisenach ist als ehemalig kreisfreie Stadt in den Wartburgkreis eingemeindet worden
             ags = 16063
-        valid_votes[2024].append(df_eu24["Gültige Stimmen"].loc[df_eu24["Nr"] == ags].item())
-        abs_votes_linke[2024].append(df_eu24["DIE LINKE"].loc[df_eu24["Nr"] == ags].item())
-        electorate[2024].append(df_eu24["Wahlberechtigte"].loc[df_eu24["Nr"] == ags].item())
-        num_voters[2024].append(df_eu24["Wählende"].loc[df_eu24["Nr"] == ags].item())
+        valid_votes["EU24"].append(df_eu24["Gültige Stimmen"].loc[df_eu24["Nr"] == ags].item())
+        abs_votes_linke["EU24"].append(df_eu24["DIE LINKE"].loc[df_eu24["Nr"] == ags].item())
+        electorate["EU24"].append(df_eu24["Wahlberechtigte"].loc[df_eu24["Nr"] == ags].item())
+        num_voters["EU24"].append(df_eu24["Wählende"].loc[df_eu24["Nr"] == ags].item())
     else:
         print("ERROR: AGS not found in results from 2024!")
-    rel_votes_linke_19 = 100 * abs_votes_linke[2019][-1] / valid_votes[2019][-1]
-    rel_votes_linke_24 = 100 * abs_votes_linke[2024][-1] / valid_votes[2024][-1]
+    if (df_LSA24["Schlüsselnummer"] == ags).any():
+        valid_votes["KOM24"].append(df_LSA24["D - Gültige Stimmen"].loc[df_LSA24["Schlüsselnummer"] == ags].item())
+        abs_votes_linke["KOM24"].append(df_LSA24["D03 - DIE LINKE"].loc[df_LSA24["Schlüsselnummer"] == ags].item())
+        electorate["KOM24"].append(df_LSA24["A - Wahlberechtigte"].loc[df_LSA24["Schlüsselnummer"] == ags].item())
+        num_voters["KOM24"].append(df_LSA24["B - Wähler"].loc[df_LSA24["Schlüsselnummer"] == ags].item())
+    elif (df_SN24["WK-Nr"] == ags).any():
+        valid_votes["KOM24"].append(df_SN24["gültige Stimmen"].loc[df_SN24["WK-Nr"] == ags].item())
+        abs_votes_linke["KOM24"].append(df_SN24["DIE LINKE"].loc[df_SN24["WK-Nr"] == ags].item())
+        electorate["KOM24"].append(df_SN24["Wahlberechtigte"].loc[df_SN24["WK-Nr"] == ags].item())
+        num_voters["KOM24"].append(df_SN24["Wähler"].loc[df_SN24["WK-Nr"] == ags].item())
+    elif (df_BB24["Gebietsschlüssel"] == ags).any():
+        valid_votes["KOM24"].append(df_BB24["Gültige Stimmen"].loc[df_BB24["Gebietsschlüssel"] == ags].item())
+        abs_votes_linke["KOM24"].append(df_BB24["DIE LINKE"].loc[df_BB24["Gebietsschlüssel"] == ags].item())
+        electorate["KOM24"].append(df_BB24["Wahlberechtigte insgesamt"].loc[df_BB24["Gebietsschlüssel"] == ags].item())
+        num_voters["KOM24"].append(df_BB24["Wählende"].loc[df_BB24["Gebietsschlüssel"] == ags].item())
+    else:
+        valid_votes["KOM24"].append(np.nan)
+        abs_votes_linke["KOM24"].append(np.nan)
+        electorate["KOM24"].append(np.nan)
+        num_voters["KOM24"].append(np.nan)
+    rel_votes_linke_19 = 100 * abs_votes_linke["EU19"][-1] / valid_votes["EU19"][-1]
+    rel_votes_linke_24 = 100 * abs_votes_linke["EU24"][-1] / valid_votes["EU24"][-1]
     diff_votes_linke.append(rel_votes_linke_24 - rel_votes_linke_19)
-    results_linke_24.append(rel_votes_linke_24)
-    lost_voters_24.append(num_voters[2024][-1] * (rel_votes_linke_24 - rel_votes_linke_19) / 100)
-elect_turnout_19 = 100.0 * sum(num_voters[2019]) / sum(electorate[2019])
-elect_turnout_24 = 100.0 * sum(num_voters[2024]) / sum(electorate[2024])
-avg_eu19 = 100.0 * sum(abs_votes_linke[2019]) / sum(valid_votes[2019])
-avg_eu24 = 100.0 * np.nansum(abs_votes_linke[2024]) / np.nansum(valid_votes[2024])
+    lost_voters_24.append(num_voters["EU24"][-1] * (rel_votes_linke_24 - rel_votes_linke_19) / 100)
+elect_turnout_19 = 100.0 * sum(num_voters["EU19"]) / sum(electorate["EU19"])
+elect_turnout_24 = 100.0 * sum(num_voters["EU24"]) / sum(electorate["EU24"])
+avg_eu19 = 100.0 * sum(abs_votes_linke["EU19"]) / sum(valid_votes["EU19"])
+avg_eu24 = 100.0 * np.nansum(abs_votes_linke["EU24"]) / np.nansum(valid_votes["EU24"])
 
 #print summary
 print("Wahlergebnisse DIE LINKE bundesweit:")
@@ -83,10 +104,11 @@ print("  Anzahl positive: " + str(sum(np.array(diff_votes_linke) >= 0)))
 
 #add electoral data to the geopraphical dataframe
 dfgeo["diff_votes_linke"] = diff_votes_linke
-dfgeo["results_linke_24"] = results_linke_24
+dfgeo["results_linke_24"] = [100 * x / y for x, y in zip(abs_votes_linke["EU24"], valid_votes["EU24"])]
+dfgeo["results_linke_kom"] = [100 * x / y for x, y in zip(abs_votes_linke["KOM24"], valid_votes["KOM24"])]
 dfgeo["lost_voters_24"] = list(map(round, lost_voters_24))
 dfgeo["linke_24_germany"] = [avg_eu24] * len(dfgeo)
-dfgeo["election_turnout"] = [100.0 * x / y for x, y in zip(num_voters[2024], electorate[2024])]
+dfgeo["election_turnout"] = [100.0 * x / y for x, y in zip(num_voters["EU24"], electorate["EU24"])]
 dfgeo["turnout_germany"] = [elect_turnout_24] * len(dfgeo)
 
 #reindexing the dataframe in order to achieve a better look
@@ -96,45 +118,62 @@ dfgeo = dfgeo.reindex(list(i0) + list(i1))
 #print(dfgeo)
 
 #plot maps with election results and changes with reference to 2019
-fig, axs = plt.subplots(1, 2, figsize=(15, 10))
+def plot_maps_to_file(plot_mode: int, title_left, title_right, text_left = "", text_right = ""):
+    assert(plot_mode == 0 or plot_mode == 1)
+    fig, axs = plt.subplots(1, 2, figsize=(15, 10))
 
-cn = int(math.ceil(np.nanmax(results_linke_24)))
-dfgeo.plot(
-    ax=axs[0],
-    alpha=0.7,
-    column="results_linke_24",
-    linewidth=0.1,
-    edgecolor="grey",
-    categorical=False,
-    legend=True,
-    cmap="Reds",
-    norm=mpl.colors.Normalize(vmin=0, vmax=cn),
-    missing_kwds=dict(color="white")
-)
-axs[0].set_xticks([])
-axs[0].set_yticks([])
-axs[0].set_title("LINKE: Ergebnisse der EU-Wahl 2024 in %")
-axs[0].text(5.7, 47, "gesamt DE 2024: " + f"{avg_eu24:.2f}" + "% LINKE, " + f"{elect_turnout_24:.2f}" + "% Wahlbeteiligung")
+    cn = int(math.ceil(np.nanmax(dfgeo["results_linke_24"])))
+    dfgeo.plot(
+        ax=axs[0],
+        alpha=0.7,
+        column="results_linke_24",
+        linewidth=0.1,
+        edgecolor="grey",
+        categorical=False,
+        legend=True,
+        cmap="Reds",
+        norm=mpl.colors.Normalize(vmin=0, vmax=cn),
+        missing_kwds=dict(color="white")
+    )
+    axs[0].set_xticks([])
+    axs[0].set_yticks([])
+    axs[0].set_title(title_left)
+    axs[0].text(5.7, 47, text_left)
 
-cn = int(math.ceil(np.nanmax(list(map(abs, diff_votes_linke)))))
-dfgeo.plot(
-    ax=axs[1],
-    alpha=0.7,
-    column="diff_votes_linke",
-    linewidth=0.1,
-    edgecolor="grey",
-    categorical=False,
-    legend=True,
-    cmap="coolwarm", #"seismic"
-    norm=mpl.colors.Normalize(vmin=-cn, vmax=cn),
-    missing_kwds=dict(color="white")
-)
-axs[1].set_xticks([])
-axs[1].set_yticks([])
-axs[1].set_title("LINKE: Gewinne und Verluste zur Wahl 2019 in %")
-axs[1].text(5.7, 47, "gesamt DE 2019: " + f"{avg_eu19:.2f}" + "% LINKE, " + f"{elect_turnout_19:.2f}" + "% Wahlbeteiligung")
-plt.savefig("Linke_heatmap.pdf", format="pdf", bbox_inches="tight")
-plt.savefig("Linke_heatmap.svg", format="svg", bbox_inches="tight")
+    col_name = "diff_votes_linke" if plot_mode==0 else "results_linke_kom"
+    cn = int(math.ceil(np.nanmax(abs(dfgeo[col_name]))))
+    vmin = -cn if plot_mode==0 else 0
+    cmap = "coolwarm" if plot_mode==0 else "PuRd"
+    dfgeo.plot(
+        ax=axs[1],
+        alpha=0.7,
+        column=col_name,
+        linewidth=0.1,
+        edgecolor="grey",
+        categorical=False,
+        legend=True,
+        cmap=cmap, #"seismic"
+        norm=mpl.colors.Normalize(vmin=vmin, vmax=cn),
+        missing_kwds=dict(color="white")
+    )
+    axs[1].set_xticks([])
+    axs[1].set_yticks([])
+    axs[1].set_title(title_right)
+    axs[1].text(5.7, 47, text_right)
+
+    f_name_pdf = "Linke_heatmap.pdf" if plot_mode==0 else "Linke_heatmap_komm.pdf"
+    f_name_svg = "Linke_heatmap.svg" if plot_mode==0 else "Linke_heatmap_komm.svg"
+    plt.savefig(f_name_pdf, format="pdf", bbox_inches="tight")
+    plt.savefig(f_name_svg, format="svg", bbox_inches="tight")
+
+plot_maps_to_file(plot_mode=0,
+                  title_left="LINKE: Ergebnisse der EU-Wahl 2024 in %",
+                  title_right="LINKE: Gewinne und Verluste zur Wahl 2019 in %",
+                  text_left="gesamt DE 2024: " + f"{avg_eu24:.2f}" + "% LINKE, " + f"{elect_turnout_24:.2f}" + "% Wahlbeteiligung",
+                  text_right="gesamt DE 2019: " + f"{avg_eu19:.2f}" + "% LINKE, " + f"{elect_turnout_19:.2f}" + "% Wahlbeteiligung")
+plot_maps_to_file(plot_mode=1,
+                  title_left="LINKE: Ergebnisse der EU-Wahl 2024 in %",
+                  title_right="LINKE: Ergebnisse der Kommunal-Wahlen 2024 in %")
 
 #create interactive plot
 cn = int(math.ceil(np.nanmax([abs(d - (avg_eu24 - avg_eu19)) for d in diff_votes_linke])))
